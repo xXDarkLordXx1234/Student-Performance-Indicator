dce6820f6c889296161d78cb8126b460fb7b522d
devansh.jaiswal <devansh.jaiswal@stud.tu-darmstadt.de>
2026-03-30 13:24:29 +0200

    Data upload

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exception_tb = error_detail.exc_info()
@@ -14,3 +15,10 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
+
+if __name__ =="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
